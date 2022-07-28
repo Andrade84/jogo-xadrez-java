6318d5ac3c48737a41ec139523ab98f0ca7e611d
Bruno Albarado <brunoalbarado19@gmail.com>
2022-07-28 19:56:29 -0300

    Tratamento de excecao e limpar tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,8 +4,10 @@ import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
+import xadrez.XadrezException;
 import xadrez.XadrezPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -15,19 +17,29 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         PartidaXadrez partidaXadrez = new PartidaXadrez();
 
-        while(true){
-            UI.imprimeTabuleiro(partidaXadrez.getPieces());
-            System.out.println();
-            System.out.print("Origem: ");
-            XadrezPosition origem = UI.lerPosicaoNoXadrez(sc);
+        while(true) {
+            try {
+                UI.clearScreen();
+                UI.imprimeTabuleiro(partidaXadrez.getPieces());
+                System.out.println();
+                System.out.print("Origem: ");
+                XadrezPosition origem = UI.lerPosicaoNoXadrez(sc);
 
-            System.out.println();
-            System.out.print("Destino: ");
-            XadrezPosition destino = UI.lerPosicaoNoXadrez(sc);
+                System.out.println();
+                System.out.print("Destino: ");
+                XadrezPosition destino = UI.lerPosicaoNoXadrez(sc);
 
-            PecaXadrez pecaCapturada = partidaXadrez.movimentarPeca(origem,destino);
+                PecaXadrez pecaCapturada = partidaXadrez.movimentarPeca(origem, destino);
+            }
+            catch (XadrezException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
-
 
     }
 }
